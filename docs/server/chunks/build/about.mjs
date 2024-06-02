4123c5bd68d1a792622d26a72cf0fdfdd9416f01
thednp <thednp@users.noreply.github.com>
2024-06-02 22:59:18 +0300

    Delete docs directory

--- docs/server/chunks/build/about.mjs
+++
@@ -1,8 +0,0 @@
-import { ssr, ssrHydrationKey, escape, createComponent } from 'solid-js/web';
-import { k } from './index22.mjs';
-import 'solid-js';
-
-var a=["<main","><!--$-->","<!--/--><h1>About</h1></main>"];function s(){return ssr(a,ssrHydrationKey(),escape(createComponent(k,{children:"About"})))}
-
-export { s as default };
-//# sourceMappingURL=about.mjs.map
